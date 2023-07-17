56a352f9f3a6276d8d695f6f2eabdb1505631eee
Pedro Henrique <henri.pedro2510@gmail.com>
2023-07-17 02:42:57 -0300

    Branchs dando trabalho

--- index.js
+++ index.js
@@ -1,39 +1,93 @@
 import express from "express";
 
-const port = 3000; // A porta que será usada pela sua aplicação.
+const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const partidas = [ {
-    titulo: "Carros",
-    local: "Desconhecido",
-    data: 2004,
-    jogadores: [],
-  },];
+let idCounter = 0;
+const partidas = [];
 
-app.get('/partidas' , (req, res) => res.send(partidas));
+app.get("/partidas", (req, res) => res.send(partidas));
 
 app.get("/partidas/:id", (req, res) => {
-    const indicePartida = partidas.findIndex((partida) => partida.id == req.params.id);
-    if (indicePartida >= 0) {
-      res.status(200).send(partidas[indicePartida]);
-    } else res.status(404).send({ mensagem: "Não foi encontrado" });
-  });
+  const partidaId = parseInt(req.params.id);
+  const partida = partidas.find((partida) => partida.id === partidaId);
+  if (partida) {
+    res.status(200).send(partida);
+  } else {
+    res.status(404).send({ mensagem: "Partida não encontrada" });
+  }
+});
 
 app.post("/partidas", (req, res) => {
-    const { titulo, local, data } = req.body;
-    const novoPartida = {
-      titulo,
-      local,
-      data,
-    };
-    partidas.push(novoPartida);
-    res.redirect("/");
-  }); 
+  const { titulo, local, data } = req.body;
+  const novaPartida = {
+    id: ++idCounter,
+    titulo,
+    local,
+    data,
+    jogadores: [],
+  };
+  partidas.push(novaPartida);
+  res.redirect("/");
+});
 
-  app.listen(port, () => {
-    console.log(`Executando em http://localhost:${port}`);
-  });
+app.post("/partidas/:id/jogadores", (req, res) => {
+  const partidaId = parseInt(req.params.id);
+  const partida = partidas.find((partida) => partida.id === partidaId);
+  if (partida) {
+    const { nome, telefone } = req.body;
+    const novoJogador = {
+      id: ++idCounter,
+      nome,
+      telefone,
+    };
+    partida.jogadores.push(novoJogador);
+    res.status(201).send({ mensagem: "Jogador adicionado com sucesso" });
+  } else {
+    res.status(404).send({ mensagem: "Partida não encontrada" });
+  }
+});
+
+app.delete("/partidas/:partidaId/jogadores/:jogadorId", (req, res) => {
+  const partidaId = parseInt(req.params.partidaId);
+  const jogadorId = parseInt(req.params.jogadorId);
+  const partida = partidas.find((partida) => partida.id === partidaId);
+  if (partida) {
+    const index = partida.jogadores.findIndex((jogador) => jogador.id === jogadorId);
+    if (index !== -1) {
+      partida.jogadores.splice(index, 1);
+      res.status(200).send({ mensagem: "Jogador removido com sucesso" });
+    } else {
+      res.status(404).send({ mensagem: "Jogador não encontrado" });
+    }
+  } else {
+    res.status(404).send({ mensagem: "Partida não encontrada" });
+  }
+});
+
+app.patch("/partidas/:partidaId/jogadores/:jogadorId", (req, res) => {
+  const partidaId = parseInt(req.params.partidaId);
+  const jogadorId = parseInt(req.params.jogadorId);
+  const confirmado = req.body.confirmado;
+
+  const partida = partidas.find((partida) => partida.id === partidaId);
+  if (partida) {
+    const jogador = partida.jogadores.find((jogador) => jogador.id === jogadorId);
+    if (jogador) {
+      jogador.confirmado = confirmado;
+      res.status(200).send({ mensagem: "Status de presença atualizado com sucesso" });
+    } else {
+      res.status(404).send({ mensagem: "Jogador não encontrado" });
+    }
+  } else {
+    res.status(404).send({ mensagem: "Partida não encontrada" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Executando em http://localhost:${port}`);
+});
--- public/index.js
+++ public/index.js
@@ -8,9 +8,123 @@ window.addEventListener("load", () => {
 
 function carregarPartidas(partidas) {
   const listaPartidas = document.querySelector("#partidas");
+
   for (const partida of partidas) {
     const itemPartida = document.createElement("li");
-    itemPartida.textContent = `${partida.titulo} - ${partida.local} - ${partida.data} `;
+    itemPartida.textContent = `${partida.titulo} - ${partida.local} - ${partida.data}`;
+
+    const idPartida = document.createElement("span");
+    idPartida.textContent = `ID: ${partida.id}`;
+    itemPartida.appendChild(idPartida);
+
+    const listaParticipantes = document.createElement("div");
+    listaParticipantes.id = `participantes-${partida.id}`;
+
+    for (const participante of partida.jogadores) {
+      const itemParticipante = document.createElement("li");
+      itemParticipante.textContent = `${partida.id}: ${participante.nome} - ${participante.telefone}`;
+
+      const confirmarPresencaCheckbox = document.createElement("input");
+      confirmarPresencaCheckbox.type = "checkbox";
+      confirmarPresencaCheckbox.checked = participante.confirmado; // Define o estado do checkbox com base no status de confirmação
+      confirmarPresencaCheckbox.addEventListener("click", () => {
+        atualizarPresenca(partida.id, participante.id, confirmarPresencaCheckbox.checked);
+      });
+
+      const confirmarPresencaLabel = document.createElement("label");
+      confirmarPresencaLabel.textContent = "Confirmar Presença";
+      confirmarPresencaLabel.appendChild(confirmarPresencaCheckbox);
+
+      itemParticipante.appendChild(confirmarPresencaLabel);
+      listaParticipantes.appendChild(itemParticipante);
+    }
+
+    const adicionarParticipanteForm = document.createElement("form");
+
+    const inputNome = document.createElement("input");
+    inputNome.type = "text";
+    inputNome.placeholder = "Nome do participante";
+
+    const inputTelefone = document.createElement("input");
+    inputTelefone.type = "text";
+    inputTelefone.placeholder = "Telefone do participante";
+
+    const adicionarParticipanteBtn = document.createElement("button");
+    adicionarParticipanteBtn.type = "button";
+    adicionarParticipanteBtn.textContent = "Adicionar Participante";
+    adicionarParticipanteBtn.addEventListener("click", () => {
+      const nome = inputNome.value;
+      const telefone = inputTelefone.value;
+
+      if (nome && telefone) {
+        adicionarParticipante(partida.id, nome, telefone);
+        inputNome.value = "";
+        inputTelefone.value = "";
+      }
+    });
+
+    adicionarParticipanteForm.appendChild(inputNome);
+    adicionarParticipanteForm.appendChild(inputTelefone);
+    adicionarParticipanteForm.appendChild(adicionarParticipanteBtn);
+
+    const removerListaBtn = document.createElement("button");
+    removerListaBtn.textContent = "Remover Lista";
+    removerListaBtn.addEventListener("click", () => {
+      const confirmacao = confirm("Tem certeza de que deseja remover a lista de participantes?");
+
+      if (confirmacao) {
+        removerLista(partida.id);
+      }
+    });
+
+    itemPartida.appendChild(listaParticipantes);
+    itemPartida.appendChild(adicionarParticipanteForm);
+    itemPartida.appendChild(removerListaBtn);
+
     listaPartidas.appendChild(itemPartida);
   }
+}
+
+function atualizarPresenca(partidaId, jogadorId, confirmado) {
+  fetch(`http://localhost:3000/partidas/${partidaId}/jogadores/${jogadorId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ confirmado }),
+  })
+    .then((resposta) => resposta.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((erro) => console.error(erro));
+}
+
+function adicionarParticipante(partidaId, nome, telefone) {
+  fetch(`http://localhost:3000/partidas/${partidaId}/jogadores`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nome, telefone }),
+  })
+    .then((resposta) => resposta.json())
+    .then((data) => {
+      const listaParticipantes = document.querySelector(`#participantes-${partidaId}`);
+      const itemParticipante = document.createElement("li");
+      itemParticipante.textContent = `${partidaId}: ${nome} - ${telefone}`;
+      listaParticipantes.appendChild(itemParticipante);
+    })
+    .catch((erro) => console.error(erro));
+}
+
+function removerLista(partidaId) {
+  fetch(`http://localhost:3000/partidas/${partidaId}/jogadores`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      const listaParticipantes = document.querySelector(`#participantes-${partidaId}`);
+      listaParticipantes.innerHTML = "";
+    })
+    .catch((erro) => console.error(erro));
 }
